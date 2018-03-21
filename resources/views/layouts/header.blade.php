af9b6d640fefd8892961db2ee1fd5785dbd86153
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-21 09:17:30 -0500

    navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,9 +7,9 @@
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-menu"></span>
           </button>
-{{--                          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                            <ul class="navbar-nav">
-                                <li class="nav-item dropdown">
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                              <ul class="navbar-nav">
+{{--                                <li class="nav-item dropdown">
                                     <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                  Explore
                  <span class="icon-arrow-down"></span>
